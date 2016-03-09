8bf003f03a4881fed6715c40a289a27e42f434cb
Michael Berkovich <michael@translationexchange.com>
2016-03-09 15:46:16 -0800

    updated dependencies

--- src/main/java/com/translationexchange/samples/spring/welp/listeners/WelpListener.java
+++ src/main/java/com/translationexchange/samples/spring/welp/listeners/WelpListener.java
@@ -1,3 +1,34 @@
+/**
+ * Copyright (c) 2016 Translation Exchange, Inc. All rights reserved.
+ *
+ *  _______                  _       _   _             ______          _
+ * |__   __|                | |     | | (_)           |  ____|        | |
+ *    | |_ __ __ _ _ __  ___| | __ _| |_ _  ___  _ __ | |__  __  _____| |__   __ _ _ __   __ _  ___
+ *    | | '__/ _` | '_ \/ __| |/ _` | __| |/ _ \| '_ \|  __| \ \/ / __| '_ \ / _` | '_ \ / _` |/ _ \
+ *    | | | | (_| | | | \__ \ | (_| | |_| | (_) | | | | |____ >  < (__| | | | (_| | | | | (_| |  __/
+ *    |_|_|  \__,_|_| |_|___/_|\__,_|\__|_|\___/|_| |_|______/_/\_\___|_| |_|\__,_|_| |_|\__, |\___|
+ *                                                                                        __/ |
+ *                                                                                       |___/
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package com.translationexchange.samples.spring.welp.listeners;
 
 import com.translationexchange.core.Tml;
@@ -7,28 +38,23 @@ import com.translationexchange.j2ee.listeners.TmlListener;
 public class WelpListener extends TmlListener {
 
 	protected void configureTml() {
-
 		Tml.getConfig().setApplication(Utils.buildMap(
-//			"key",		"05ebcb1f085f8e2b97b572b5efe61f056159cbbb72b79ef2989bd7260d0c7001",
-//			"token", 	"c41b8ffaf8c98eb290f9cb9d353afbb72d48a14ecda2e3eacda2b581948a8f2f",
-//			"host", 	"http://localhost:3000"
-			"key",		"0332a07e97af3d81f6fe9723cc083df3a45fbd39d1380691c62e445c598b8d5c",
-			"token", 	"0d94597cec4aa324ac764f53f6cb0e33ef5b3954144364f91f27fde8e8fc36fb"
-		));
-
-//		Tml.getConfig().setCache(Utils.buildMap(
-//			"enabled",  true,	
-//	    	"class", 	"com.translationexchange.cache.Memcached",
-//	        "host", 	"localhost:11211",
-//	        "namespace", "c41b8ffaf8c98"
-//		));
-
-		Tml.getConfig().setCache(Utils.buildMap(
-				"enabled",  false,	
-		    	"class", 	"com.translationexchange.cache.EHCache",
-		        "namespace", "0332a07e97af3d81f6fe9723cc083df3a45fbd39d1380691c62e445c598b8d5c"
+			"key", "e9ecde2f25c1fed5c555e4bb036210fcde472536f5d71cc8b85f3508de9c845c",	
+			"host", "http://localhost:3000",
+			"cdn_host", "https://trex-snapshots-dev.s3-us-west-1.amazonaws.com"
 		));
 		
+		Tml.getConfig().setAgent(Utils.buildMap(
+			"host", "http://localhost:8282/dist/agent.js"
+		));
+	
+		Tml.getConfig().setCache(Utils.buildMap(
+			"enabled", 				true,	
+	    	"class", 				"com.translationexchange.cache.Memcached",
+	        "host", 				"localhost:11211",
+	        "namespace", 			"e9ecde2f25c1",
+	        "version_check_interval", 10
+		));	
 	}
 	
 }
